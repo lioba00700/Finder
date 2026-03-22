cc4fdc8c6b2843e475b34cad6b796228b35fbd04
lioba00700 <lioba00700@gmail.com>
2026-03-22 22:28:26 +0900

    init: capacitor 세팅

---
+++ apps/app/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from "@capacitor/cli";
+
+const config: CapacitorConfig = {
+  appId: "com.finder.app",
+  appName: "Finder",
+  webDir: "out",
+};
+
+export default config;
