7463cf8fb69977dae112be93dbe753947e207e4e
lioba00700 <lioba00700@gmail.com>
2026-04-02 23:30:30 +0900

    feat: lucide icon 추가

--- apps/app/src/widgets/layouts/bottom-nav.tsx
+++ apps/app/src/widgets/layouts/bottom-nav.tsx
@@ -1,22 +1,23 @@
 "use client";
 
+import { Home, Paperclip } from "lucide-react";
 import { useRouter } from "next/navigation";
 
 export function BottomNav() {
   const router = useRouter();
   return (
     <nav className="bottom-0 left-0 right-0 h-15 border-gray-200 bg-white">
-      <div className="flex relative justify-around h-full items-center">
+      <div className="flex relative justify-center gap-20 h-full items-center">
         {[
-          { name: "홈", href: "/main" },
-          { name: "업로드", href: "/upload" },
+          { icon: <Home />, href: "/main" },
+          { icon: <Paperclip />, href: "/upload" },
         ].map((item, index) => (
           <button
             key={index}
             onClick={() => router.push(item.href)}
-            className="flex-1 text-center py-5 text-xl font-medium text-gray-700"
+            className="text-center py-5 text-xl font-medium text-gray-700"
           >
-            {item.name}
+            {item.icon}
           </button>
         ))}
       </div>
